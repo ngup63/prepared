aaca62babb275dc968b8e7177748aed9b20a50dd
ngup63 <78304600+ngup63@users.noreply.github.com>
2024-10-15 00:07:41 +0800

    Create prehost.js

---
+++ prehost.js
@@ -0,0 +1,8 @@
+[
+  "https://9ycveml4.9hedsjco5l.shop",
+  "https://7inmzhr.7o7xi77fb.shop",
+  "https://x230p2.6una41u.shop",
+  "https://s0huzz.2wrh65j1k.shop",
+  "https://58knb7d3.08goza0yr.shop",
+  "https://hzi3vbus.s3r2dhq.shop",
+]
